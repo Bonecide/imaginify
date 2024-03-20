e3347cc937ebaaa2c1fb0e38eec19fd80ebb7c82
Bonecide <marselkasymov23@gmail.com>
2024-03-20 17:58:45 +0600

    fixed login button

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -94,7 +94,7 @@ const Sidebar = () => {
           </SignedIn>
           <SignedOut>
             <Button asChild className="button bg-purple-gradient bg-cover">
-              <Link href="/sign-in"></Link>
+              <Link href="/sign-in">Login</Link>
             </Button>
           </SignedOut>
         </nav>
